af8ca7d9ec6faf834365c8e3386214e3b370d19a
Michael Hoobler <hoobler.michael@gmail.com>
2019-01-17 17:28:22 -0500

    fixed signup problems??

--- assets/javascript/signup.js
+++ assets/javascript/signup.js
@@ -24,7 +24,22 @@ $("#register").on("click", function(){
     }).then(function() {
       if(!errorCheck){
         //fix moving from signup
-        window.location.href = "virtualproject2.html";
+        var userEmail = auth.currentUser.email;
+        var userID = auth.currentUser.uid;
+        var postData = {
+          email: userEmail,
+          joinDate: "11-11-11",
+          testArray: []
+        };
+      
+        // Get a key for a new Post.
+        var newPostKey = firebase.database().ref().child('posts').push().key;
+      
+        // Write the new post's data simultaneously in the posts list and the user's post list.
+        var updates = {};
+        updates['/users/' + userID] = postData;
+      
+        return firebase.database().ref().update(updates).then(function() {window.location.href = "virtualproject2.html"});
       }
     });;
   
